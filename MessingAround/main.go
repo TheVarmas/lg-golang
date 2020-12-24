9c7fe0e4056142d6fae98b61d73e39b963a479c0
Lightgoddess <aanya.varma@outlook.com>
2020-12-24 00:41:06 +0000

    Prints a var returned by a func

---
+++ MessingAround/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Program running...")
+
+    someVariable := function()
+    fmt.Println(someVariable)
+}
+
+func function() int {
+    variable:= 1
+    return variable
+}
