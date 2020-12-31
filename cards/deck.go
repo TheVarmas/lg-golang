57638f80490b574552ddf92fceca9b960c7aed68
Lightgoddess <aanya.varma@outlook.com>
2020-12-31 17:21:36 +0000

    Implemented all the file operations and helper functions

---
+++ cards/deck.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
+type deck []string
+
+func createDeck() deck {
+    values := [] string {
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    }
+
+    suits := []string {
+        "Diamonds",
+        "Hearts",
+        "Clubs",
+        "Spades",
+    }
+    
+    var someDeck deck
+
+
+    for _, value := range values{
+        for _, suit := range suits{
+            someDeck = append(someDeck, value + " of " + suit)
+        }
+    }
+
+    return someDeck
+}
+
+func (d deck) showCards() {
+    for _, card := range d {
+        fmt.Println(card)
+    }
+}
+
+func (d deck) deal (handSize int) (deck, deck){
+    return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+    return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile (fileName string) {
+    ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func toDeck (ds string) deck {
+    return deck(strings.Split(ds, ","))
+    
+}
+
+// First attempt that has gone wrong!
+// [TODO] Retry with mutiple lines and reverse saveToFile
+
+// func readFromFileBad(nameFile string) deck {
+//     return deck(string(ioutil.ReadFile(nameFile)))
+// }
+
+// 2nd attempt
+func readFromFile (fileName string) deck {
+    bs, err := ioutil.ReadFile(fileName)
+
+    if err != nil {
+        fmt.Println("Error: ", err)
+    }
+
+    return toDeck(string(bs))
+}
---
+++ cards/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+    newDeck := createDeck()
+    fmt.Println("\n   This is a deck: \n")
+    newDeck.showCards()
+    hand, remainingDeck := newDeck.deal(26)
+    fmt.Println("\n   This is the hand: \n")
+    hand.showCards()
+    fmt.Println("\n   This is the remaining deck: \n")
+    remainingDeck.showCards()
+
+    newDeck.saveToFile("Deck_1")
+
+    readFromFile("My_deck")
+}
